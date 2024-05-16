332dba6690498fd5c5e6de3890d1a3a37179091a
risav pyakurel <pyakurel.risav@gmail.com>
2024-05-16 22:17:05 +0545

    recognizing int

--- basic.py
+++ basic.py
@@ -1,121 +1,104 @@
 # Declaring digits constant
-
 DIGITS = '0123456789'
 
-
-# creating an error class
-
+# Creating an error class
 class Error:
-    def __init__(self,error_name, details):
+    def __init__(self, error_name, details):
         self.error_name = error_name
         self.details = details
-    def as_string(self):
-         result = f'{self.error_name}: {self.details}'
-         return result
 
-class Illegal_character_Error(Error): #Illegal characterError is standard error  used when lexer comes across the char that doesn't support
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        return result
+
+class IllegalCharacterError(Error):
     def __init__(self, details):
         super().__init__('Illegal Character', details)
 
-
-# working on token class
-
-TT_INT= 'TT_INT'
+# Working on token class
+TT_INT = 'INT'
 TT_FLOAT = 'FLOAT'
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
-TT_MUL= 'MUL'
+TT_MUL = 'MUL'
 TT_DIV = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 
-class Token :
-    def __init__(self,type_, value):
+class Token:
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
-        if self.value: return f'{self.type} : {self.value}'
+        if self.value:
+            return f'{self.type}:{self.value}'
         return f'{self.type}'
 
-
-#now we work on lexer class
+# Now we work on lexer class
 class Lexer:
-    def __init__(self,text):
-        self.text =text
+    def __init__(self, text):
+        self.text = text
         self.position = -1
         self.current_character = None
         self.advance()
 
-
     def advance(self):
-        self.position +=1
-        self.current_character = self.text[position] if self.position < len(self.text) else None
+        self.position += 1
+        self.current_character = self.text[self.position] if self.position < len(self.text) else None
 
-        def make_tokens(self):
-            tokens= []
-            while self.current_character != None:
-                if self.current_character in ' \t':
-                    self.advance()
-                elif self.current_character in DIGITS:
-                    tokens.append(self.make_number())
-
-                elif self.current_character == '+':
-                    tokens.append(Token(TT_PLUS))
-                    self.advance()
-
-                elif self.current_character == '*':
-                    tokens.append(Token(TT_MUL))
-                    self.advance()
-
-                elif self.current_character == '/':
-                    tokens.append(Token(TT_DIV))
-                    self.advance()
-
-                elif self.current_character == '(':
-                    tokens.append(Token(TT_LPAREN))
-                    self.advance()
-
-                elif self.current_character == ')':
-                    tokens.append(Token(TT_RPAREN))
-                    self.advance()
-
-                else:    #   if we don't find the character we are looking for we have to throw the error
-                     char= self.current_character
-                     self.advance()
-                     return [], Illegal_character_Error( "'" +char + "'")
-                
-                
-
-
-
-
-
-
-
-
-            return tokens
-
-        def make_number(self):
-            num_str= ''
-            dot_count= 0
-
-            while self.current_character != None and self.current_character in DIGITS + '.':
-                if self.current_character == '.':
-                    dot_count +=1
-                    num_str += '.'
-
-                else:
-                    num_str += self.current_char
-
-            if dot_count == 0:
-                return Token(TT_INT, int(num_str))
+    def make_tokens(self):
+        tokens = []
+        while self.current_character is not None:
+            if self.current_character in ' \t':
+                self.advance()
+            elif self.current_character in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_character == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_character == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_character == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_character == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_character == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_character == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
             else:
-                return Token(TT_FLOAT, float(num_str))
+                char = self.current_character
+                self.advance()
+                return [], IllegalCharacterError("'" + char + "'")
+        return tokens, None
 
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
 
+        while self.current_character is not None and self.current_character in DIGITS + '.':
+            if self.current_character == '.':
+                if dot_count == 1:
+                    break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_character
+            self.advance()
 
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
 
-
-
-
+# Creating a run function
+def run(text):
+    lexer = Lexer(text)
+    tokens, error = lexer.make_tokens()
+    return tokens, error
