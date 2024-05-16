6c95f632d4833d47d341198eb41d9db16f676fe2
risav pyakurel <pyakurel.risav@gmail.com>
2024-05-16 19:31:06 +0545

    defining tokens

---
+++ basic.py
@@ -0,0 +1,11 @@
+TT_INT= 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_DIV = 'DIV'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+
+class Token :
+    def __init__(self,type_, value):
+        self.type =
